a48ec71d4086ac35c985f1dffc4bfcf53eeba237
Gaurav <gauravsuryawanshi191@gmail.com>
2023-11-20 17:49:12 +0530

    day4.2

--- day4.1/src/main/java/pages/AdminServlet.java
+++ day4.1/src/main/java/pages/AdminServlet.java
@@ -3,11 +3,15 @@ package pages;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import pojos.User;
 
 /**
  * Servlet implementation class AdminServlet
@@ -20,7 +24,10 @@ public class AdminServlet extends HttpServlet {
 		System.out.println("in admin page");
 		resp.setContentType("text/html");
 		try (PrintWriter pw = resp.getWriter()) {
-			pw.print("<h4>Welcome Admin</h4>");
+			HttpSession session = req.getSession();
+			User user = (User) session.getAttribute("user_details");
+			pw.print("<h4>Welcome Admin" + user.getFirstName() + " " + user.getLastName() + "</h4>");
+			pw.write("<h4>" + user + "</h4>");
 			pw.print("<h4><a href='login.html'>Login</a></h4>");
 		}
 	}
